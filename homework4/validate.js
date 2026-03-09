af1141e0447cf7ecd2cd484790e968f13921e94d
blueflowers44 <asroy2@cougarnet.uh.edu>
2026-03-09 16:20:50 -0500

    Update validate.js

--- homework4/validate.js
+++ homework4/validate.js
@@ -34,7 +34,42 @@ console.log("Error loading state list");
     const el = $(id);
     if (el) el.textContent = msg || "";
   }
+function setCookie(name,value,days){
 
+const d = new Date();
+d.setTime(d.getTime() + (days*24*60*60*1000));
+
+document.cookie =
+name + "=" + value +
+";expires=" + d.toUTCString() +
+";path=/";
+
+}
+
+function getCookie(name){
+
+let cname = name + "=";
+
+let decodedCookie = decodeURIComponent(document.cookie);
+let ca = decodedCookie.split(';');
+
+for(let i=0;i<ca.length;i++){
+
+let c = ca[i];
+
+while(c.charAt(0)==' '){
+c = c.substring(1);
+}
+
+if(c.indexOf(cname)==0){
+return c.substring(cname.length,c.length);
+}
+
+}
+
+return "";
+
+}
   function fmtMoney(n) {
     return Number(n).toLocaleString("en-US", {
       style: "currency",
